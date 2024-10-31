c91a6610147e659cf38028b545a8c339f4d42558
Mohammad Hatami <hatamitekno@gmail.com>
2024-10-31 20:24:21 +0700

    add layanan, pengerjaan service

---
+++ app/Filament/Resources/LayananResource.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\LayananResource\Pages;
+use App\Filament\Resources\LayananResource\RelationManagers;
+use App\Models\Layanan;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class LayananResource extends Resource
+{
+    protected static ?string $model = Layanan::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-cog';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+        ->schema([
+            Forms\Components\Select::make('id_kategori')
+                ->label('Kategori')
+                ->relationship('kategori', 'nama')
+                ->required(),
+            Forms\Components\TextInput::make('nama')
+                ->label('Nama Layanan')
+                ->required()
+                ->maxLength(255),
+            Forms\Components\Select::make('status')
+                ->label('Status')
+                ->options([
+                    1 => 'Aktif',
+                    0 => 'Tidak Aktif',
+            ]),
+        ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+        ->columns([
+            Tables\Columns\TextColumn::make('kategori.nama')
+                ->label('Kategori')
+                ->numeric()
+                ->sortable(),
+            Tables\Columns\TextColumn::make('nama')
+                ->searchable(),
+            Tables\Columns\TextColumn::make('status')
+            ->label('Status')
+            ->formatStateUsing(fn ($state) => $state ? 'Aktif' : 'Tidak Aktif'),
+            Tables\Columns\TextColumn::make('created_at')
+                ->dateTime()
+                ->sortable()
+                ->toggleable(isToggledHiddenByDefault: true),
+            Tables\Columns\TextColumn::make('updated_at')
+                ->dateTime()
+                ->sortable()
+                ->toggleable(isToggledHiddenByDefault: true),
+        ])
+        ->filters([
+            //
+        ])
+        ->actions([
+            Tables\Actions\EditAction::make(),
+        ])
+        ->bulkActions([
+            Tables\Actions\BulkActionGroup::make([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]),
+        ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListLayanans::route('/'),
+            'create' => Pages\CreateLayanan::route('/create'),
+            'edit' => Pages\EditLayanan::route('/{record}/edit'),
+        ];
+    }
+}
